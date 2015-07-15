8969db9e0f3868f5c0b731f93f0fc763ad313cfe
Bruno Alfano-Rizzo <bruno.alfano.rizzo@gmail.com>
2015-07-14 23:44:00 -0400

    created express app and included module variables in index.js

---
+++ server/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var _ = require('lodash');
+
+// Create the application
+var app = express();
