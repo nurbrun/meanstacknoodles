6af89152a69830b12ddfb4a95913a16805defb9f
Bruno Alfano-Rizzo <bruno.alfano.rizzo@gmail.com>
2015-07-15 00:40:22 -0400

    added mongoDB and CORS support RESTful interfaces. Exposes APIs to URLS that are accessing the server. Now a public API

--- server/index.js
+++ server/index.js
@@ -8,6 +8,21 @@ var _ = require('lodash');
 var app = express();
 
 // Add Middleware necessary for REST API's
-app.use(bodyParser.urlencoded({etended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
+
+// CORS Support
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+});
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/meanstacknoodles');
+mongoose.connection.once('open', function() {
+	console.log('Listening on port 3000...');
+	app.listen(3000);
+});
